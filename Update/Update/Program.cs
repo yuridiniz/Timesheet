4b8a0f0e8ff4f8b9b6c3feddd463a761d3519e88
Yuri Araújo <yschmitzz@hotmail.com>
2014-07-23 13:32:40 -0300

    Update

---
+++ Update/Update/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Update
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Delete(args[0] + "\\Timesheet.exe");
+            File.Move(Environment.CurrentDirectory + "\\TimesheetNew.exe", args[0] + "\\Timesheet.exe");
+        }
+    }
+}
